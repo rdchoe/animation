cd47595da6a470d1388969915d375fbc0b4588d2
robertchoe <robert.choe@robinhood.com>
2022-11-27 00:12:37 -0500

    Initial Commit

---
+++ animation/animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by Robert Choe on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
